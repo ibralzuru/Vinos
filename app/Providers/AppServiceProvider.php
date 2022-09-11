349c7389b777b7e217f8fc1a798febf313ec176d
‘Ibra‘ <ibracortes@gmail.com>
2022-09-11 23:10:07 +0200

    heroku problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use Illuminate\Routing\UrlGenerator;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')){
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -21,8 +26,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if(env('REDIRECT_HTTPS')){
+            $url->formatSheme('https//');
+        }
     }
 }
