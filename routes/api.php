154f5f8fd7238fa35e49b513e59c4a5e71acfccb
‘Ibra‘ <ibracortes@gmail.com>
2022-09-15 14:45:38 +0200

    problem heroku

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
